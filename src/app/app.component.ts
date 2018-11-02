1e2bccb05642e19ad05e393bd370472e25968de0
Cesar Gonzalez <cesarmanuelgc@gmail.com>
2018-11-02 14:58:09 -0500

    Added source code

---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+//import 'rxjs/add/observable/timer';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  isChecked = true;
+
+  minDate = new Date(2018, 10, 1);
+  maxDate = new Date(2018, 11, 1);
+
+  categories = [
+    { name: 'Beginer' },
+    { name: 'Intermediate' },
+    { name: 'Advanced' },
+  ];
+
+  colors = [
+    { id: 1, name: 'Red' },
+    { id: 2, name: 'Green' },
+    { id: 3, name: 'Blue' }
+  ];
+
+  color = 2;
+
+  progress = 0;
+  timerInterval;
+  isLoading = false;
+
+  /**
+   *
+   */
+  constructor() {
+    // this.isLoading = true;
+    // this.getCourses()
+    //   .subscribe(x => this.isLoading = false);
+    // this.timerInterval = setInterval(() => {
+    //   this.progress++;
+    //   if (this.progress >= 100) {
+    //     clearInterval(this.timerInterval);
+    //   }
+    // }, 200);
+  }
+
+  // getCourses(): Observable<any> {
+  //   return Observable.timer(2000);
+  // }
+
+  onChange($event) {
+    console.log($event);
+  }
+
+  selectCategory(category) {
+    this.categories
+        .filter(c => c !== category)
+        .forEach(c => c['selected'] = false);
+
+        category.selected = !category.selected;
+  }
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+// import { NoopAnimationsModule } from '@angular/platform-browser/animations'; use this if we don't need animations on angular material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
